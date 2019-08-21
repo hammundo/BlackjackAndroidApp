aaabebe862df62ea31cd7a44c669aaa7beb65da0
Hammond <jsh.s.hammond@gmail.com>
2019-08-21 17:59:53 +0100

    Added unit tests for Deck class.

---
+++ app/src/test/java/com/example/blackjack_android_app/DeckTest.java
@@ -0,0 +1,54 @@
+package com.example.blackjack_android_app;
+
+import junit.extensions.RepeatedTest;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.lang.annotation.Repeatable;
+
+import static org.junit.Assert.*;
+
+public class DeckTest {
+
+    // This tests the deal() and reloadDeck() methods, as reloadDeck() is a dependency.
+    @Test
+    public void deal() {
+        final int expected = 1;
+        final int actual;
+
+        PlayerHand testHand = new PlayerHand();
+        Deck testDeck = new Deck();
+
+        testDeck.reloadDeck();
+        testHand.addCardToHand(testDeck.deal());
+
+        actual = testHand.getCardCount();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    // Due to the nature of SecureRandom, this test should pass the majority of the time.
+    // However it is very unlikely that the same card will be drawn both times.
+    @Test
+    public void shuffle() {
+        Deck testDeck = new Deck();
+
+        // Load deck
+        testDeck.reloadDeck();
+        // Assign top card
+        final Card card1 = testDeck.deal();
+
+        // Reload deck with shuffle
+        testDeck.reloadDeck();
+        // Assign top card
+        final Card card2 = testDeck.deal();
+
+        // Print card objects
+        System.out.println(card1.getName());
+        System.out.println(card2.getName());
+
+        Assert.assertNotEquals(card1.getName(), card2.getName());
+    }
+}
